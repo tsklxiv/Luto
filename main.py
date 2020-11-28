1bb525756b6e3ea00716a839c1e9e49c59f875b8
Hoang Tuan(Wumi4) <danghoangtuan526@gmail.com>
2020-11-28 20:33:29 +0700

    Update the main.py file

--- main.py
+++ main.py
@@ -16,19 +16,8 @@ from os import system
 theme = Theme({"cmd": "italic bold", "content": "bold", "error": "bold red"})
 cons = Console(theme=theme)
 todos = []
-FILE = "database.txt"
 color_list = ["red", "green", "blue", "yellow", "magenta", "bold white"]
 tags = {}
-need_to_save = False
-
-
-# Functions
-def save_todos():
-    with open(FILE, "w") as fl:
-        for todo in todos:
-            content, tag = todo
-            fl.write(f"{content}: {tag}\n")
-        fl.close()
 
 
 cons.print("Welcome to [red]Luto[/red]!")
@@ -54,12 +43,12 @@ while True:
             [cmd]delete[/cmd]: Delete a todos
             [cmd]help[/cmd]: Display this help
             [cmd]quit[/cmd] or [cmd]exit[/cmd]: Quit the program
-            [cmd]save[/cmd]: Save the todo list
             [cmd]delete_all[/cmd]: Delete all the todos
             [cmd]clear[/cmd]: Clear the screen
-            
+
             # [b]Further features:[/b]
                 [cmd]delete_tag[/cmd]: Delete every todo that has the same tag
+                [cmd]save[/cmd]: Save the todo list app into a database (Planned to use MariaDB)
         """)
     elif parts[0] == "delete":
         try:
@@ -68,16 +57,7 @@ while True:
         except Exception:
             cons.print(f"[error]Error: {Exception}[/error]")
     elif parts[0] == "quit" or parts[0] == "exit":
-        if need_to_save is False:
-            break
-        elif need_to_save is True:
-            choice = cons.input("Save?(Y/n) ")
-            if choice.lower() == "y":
-                save_todos()
-                need_to_save = False
-                break
-            elif choice.lower() == "n":
-                break
+        break
     elif parts[0] == "clear":
         system("clear")
     elif parts[0] == "add":
@@ -89,17 +69,6 @@ while True:
         todos.append((content, tag))
         cons.print("A [i]todo[/i] has been added! :smile:")
         need_to_save = True
-    elif parts[0] == "save":
-        if need_to_save is False:
-            choice = cons.input("Overwrite?(Y/n) ")
-            if choice.lower() == "y":
-                save_todos()
-                need_to_save = False
-            elif choice.lower() == "n":
-                pass
-        elif need_to_save is True:
-            save_todos()
-            need_to_save = False
     elif parts[0] == "delete_all":
         todos = []
         need_to_save = True
