643f54ff0c8759de7f688363e9b6b1e874a0691e
Wumi4 <66067893+HoangTuan110@users.noreply.github.com>
2021-01-05 15:38:41 +0700

    Update main.py
    Update the outdated main.py file

--- main.py
+++ main.py
@@ -10,27 +10,46 @@
 
 from rich.console import Console
 from rich.theme import Theme
+from rich.table import Table
 import random
 from os import system
 
-theme = Theme({"cmd": "italic bold", "content": "bold", "error": "bold red"})
+theme = Theme({"cmd": "italic", "content": "bold", "error": "bold red"})
 cons = Console(theme=theme)
 todos = []
-color_list = ["red", "green", "blue", "yellow", "magenta", "bold white"]
+color_list = ["red", "green", "blue", "yellow", "magenta"]
 tags = {}
+current_render_type = "normal"
 
 
+def render(todos, tags, render_type):
+    if render_type == "normal":
+        for todo in todos:
+            content, tag = todo
+            tag_color = tags[tag]
+            cons.print(f" - [content]{content}[/content]: [{tag_color}]{tag}[/{tag_color}]")
+    elif render_type == "board":
+        table = Table(title="Todos")
+        table.add_column("Todo", justify="left", style="magenta")
+        table.add_column("Tag", justify="right", style="magenta")
+
+        for todo in todos:
+            content, tag = todo
+            tag_color = tags[tag]
+            table.add_row(content, tag, style=tag_color)
+
+        cons.print(table)
+
+
+system("clear")
 cons.print("Welcome to [red]Luto[/red]!")
 cons.print("Type [cmd]help[/cmd] for help")
 cons.print("And [cmd]quit[/cmd] to quit")
 
 
 while True:
-    cons.print("Todos:")
-    for todo in todos:
-        content, tag = todo
-        tag_color = tags[tag]
-        cons.print(f"   - [content]{content}[/content]: [{tag_color}]{tag}[/{tag_color}]")
+    cons.print("Todos:", style="bold")
+    render(todos, tags, current_render_type)
     cons.print()
     ipt = cons.input(">>>")
     parts = ipt.split()
@@ -44,6 +63,7 @@ while True:
             [cmd]help[/cmd]: Display this help
             [cmd]quit[/cmd] or [cmd]exit[/cmd]: Quit the program
             [cmd]delete_all[/cmd]: Delete all the todos
+            [cmd]clear[/cmd]: Clear the screen.
 
             # [b]Further features:[/b]
                 [cmd]delete_tag[/cmd]: Delete every todo that has the same tag
@@ -52,8 +72,8 @@ while True:
         try:
             todo_to_delete = int(parts[1])
             todos.pop(todo_to_delete)
-        except Exception:
-            cons.print(f"[error]Error: {Exception}[/error]")
+        except Exception as e:
+            cons.print(f"[error]Error: {e}[/error]")
     elif parts[0] == "quit" or parts[0] == "exit":
         break
     elif parts[0] == "clear":
@@ -72,3 +92,4 @@ while True:
         need_to_save = True
     else:
         cons.print(f"[error]Error: Invalid command: {parts[0]}[/error]")
+
